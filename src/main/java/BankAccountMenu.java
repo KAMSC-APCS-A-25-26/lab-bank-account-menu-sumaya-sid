1cf992a0677452d9dc6f29166649329b2ff577fa
sumaya-sid <sumaya.siddiqui49024@gmail.com>
2025-09-21 19:44:31 -0400

    Update BankAccountMenu.java

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -22,7 +22,7 @@ public class BankAccountMenu {
             System.out.println("4. Exit");
 
             // 4. Use Scanner to read user input
-            System.out.print("Enter your choice:  ");;
+            System.out.print("Enter your choice: ");
             int choice = sc.nextInt();
 
             // 5. Use switch statement to handle menu choices
@@ -30,7 +30,7 @@ public class BankAccountMenu {
             {
                 // 6. Implement add money, withdraw money, check balance, and exit functionality
                 case 1:
-                    System.out.print("Enter amount to add:  ");
+                    System.out.print("Enter amount to add: ");
                     double amoney = sc.nextDouble();
                     if (amoney <= 0)
                     {
@@ -44,7 +44,7 @@ public class BankAccountMenu {
                     break;
 
                 case 2:
-                    System.out.print("Enter amount to withdraw:  ");
+                    System.out.print("Enter amount to withdraw: ");
                     double wmoney = sc.nextDouble();
                     if (wmoney <= 0)
                     {
@@ -57,16 +57,16 @@ public class BankAccountMenu {
                     else
                     {
                         account -= wmoney;
-                        System.out.printf("New balance:  $%.2f\n", account);
+                        System.out.printf("New balance: $%.2f\n", account);
                     }
                     break;
 
                 case 3:
-                    System.out.printf("Current Balance:  $%.2f\n", account);
+                    System.out.printf("Current Balance: $%.2f\n", account);
                     break;
 
                 case 4:
-                    System.out.print("Goodbye!");
+                    System.out.println("Goodbye!");
                     run = false;
                     break;
 
